6598602c4bc5a6a6cc49e30169efee31c48df923
Gian Masto <gianmasto1@gmail.com>
2023-02-25 14:56:20 -0300

    added puppeteer to generate pdf

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const express = require('express')
 const multer = require('multer')
+const cors = require('cors')
+const fs = require('fs')
+const ejs = require('ejs')
+const puppeteer = require('puppeteer')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +21,7 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage })
 
+app.use(cors())
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
@@ -25,7 +30,42 @@ app.get('/', (req, res) => {
 })
 app.post('/report', upload.any(), (req, res) => {
   const data = {isAdmin: true, ...req.body, files:{...req.files}}
-  res.render('pages/report', {data})
+  const firma64 = data.firma.split(',')[1]
+  const firmaDecoded = Buffer.from(firma64, 'base64').toString('utf8') 
+
+  data.firma = firmaDecoded
+  ejs.renderFile('views/pages/report.ejs', {data}, {} ,function(err, str){
+      if (err) {
+        console.log(err)
+      } else {
+        const randomFileName = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        fs.writeFile(`./public/pdf/pdf.html`, str, (err) => {
+          if (err) throw err;
+          console.log('File is created successfully.');
+
+          (async () => {
+            const browser = await puppeteer.launch({executablePath: '/opt/homebrew/bin/chromium', args: [ '--disable-gpu', '--disable-setuid-sandbox', '--no-sandbox', '--no-zygote' ]})
+            const page = await browser.newPage()
+            const website_url = 'http://localhost:3000/pdf/pdf.html'
+
+            await page.goto(website_url, { waitUntil: 'networkidle0' });
+            await page.emulateMediaType('screen');
+            const pdf = await page.pdf({
+              path: 'result.pdf',
+              margin: { top: '50px', right: '50px', bottom: '50px', left: '50px' },
+              printBackground: true,
+              format: 'A4', 
+            });
+            await browser.close();
+
+          })()
+
+
+        })
+      }
+  });
+
+  res.redirect('/')
 })
 
 app.listen(port, ["192.168.1.37", "localhost" ], () => {
