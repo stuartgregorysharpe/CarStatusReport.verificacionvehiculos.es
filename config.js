31eea886a719d776535d99e87658ae58c4ec042d
Gian Masto <gianmasto1@gmail.com>
2023-05-14 16:01:56 -0300

    chromium path now is getted from env variable

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 module.exports = {
   ROOT_PATH: __dirname,
   PORT: 3000,
-  USERS_JSON_PATH: './usuarios.json',
-  CHROMIUM_PATH: '/opt/homebrew/bin/chromium', // '/usr/bin/chromium-browser', // macos: /opt/homebrew/bin/chromium
+  CHROMIUM_PATH: process.env.CHORMIUM_PATH,
   SALT: 'QOolyjHOm1h84VZjTnLdrynxZkU+qXP0YoZ/tRCxeSD+00td58WGRVuK1its5chmKT3rfqzbqvgnbBmVg2rmyLIOwhXVj75JnBM7ypaIodkgt+6rfhmKzbHrY+0wpscLcJBDYfkycGBmIJ9mBV/51rZSszE43SMK8EMv/bRyoOc=',
   EMAIL: process.env.APP_EMAIL,
   EMAIL_PASSWORD: process.env.APP_EMAIL_PASSWORD,
